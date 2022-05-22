8c3d50a64fda261f9eb96a0fcfd0664eed3d61a9
Jullia Casimbon <ijcasimbon@gmail.com>
2022-05-22 18:01:20 -0400

    finished conservation Efforts

--- scripts/howToHelp.js
+++ scripts/howToHelp.js
@@ -1,7 +1,7 @@
 function hideDef() {
     var topic = document.getElementsByTagName("div");
-    for(i=1; i< topic.length; i++){
-        if(i == 1){
+    for(i=2; i< topic.length; i++){
+        if(i == 2){
             document.getElementById(topic[i].id).style.display = "block"
         }else{
             document.getElementById(topic[i].id).style.display = "none"
@@ -11,7 +11,7 @@ function hideDef() {
 
 function viewTopic(str) {
     var topic = document.getElementsByTagName("div");
-    for(i=1; i< topic.length; i++){
+    for(i=2; i< topic.length; i++){
         if(topic[i].id == str){
             document.getElementById(topic[i].id).style.display = "block"
         }else{
